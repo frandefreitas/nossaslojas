93e2ca8ee3a40e6f0dc0f9a9e1380f99b3aef7f5
Francisco de Freitas Kemle <francisco.kemle@pampa.compasso>
2019-04-09 09:01:06 -0300

    Creating first route

--- config/custom-express.js
+++ config/custom-express.js
@@ -1,10 +1,12 @@
 var express = require('express');
 var consign = require('consign');
 
-module.exports = function() {
+module.exports = function(){
+  var app = express();
 
-    var app = express();
-    consign().into(app);
+  consign()
+   .include('controllers')
+   .into(app);
 
-    return app;
+  return app;
 }
